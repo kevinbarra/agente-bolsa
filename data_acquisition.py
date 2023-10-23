7772e9e8d4f8888cb1fb03bdf4d9761c0e8554a3
kevinbarra <98494916+kevinbarra@users.noreply.github.com>
2023-10-23 10:56:07 -0600

    Update data_acquisition.py

--- data_acquisition.py
+++ data_acquisition.py
@@ -5,13 +5,10 @@ def get_data_from_yahoo(ticker, start_date, end_date):
     """
     Descarga datos históricos para un ticker específico desde Yahoo Finance entre las fechas especificadas.
 
-    Parámetros:
-    - ticker (str): El símbolo del activo en Yahoo Finance (por ejemplo, "AAPL" para Apple).
-    - start_date (str): Fecha de inicio en formato "AAAA-MM-DD".
-    - end_date (str): Fecha final en formato "AAAA-MM-DD".
 
-    Retorna:
-    - DataFrame: Conjunto de datos históricos con las columnas de Fecha, Open, High, Low, Close, Adj Close y Volume.
+
+
+  
     """
     
     # Utilizar la función download de yfinance para obtener los datos históricos
